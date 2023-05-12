c682af435d8706b3753b9ebd34b4cbfa21a092ba
mohammed qasim <87497268+MohammedTsmu@users.noreply.github.com>
2023-05-12 13:38:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ async function generateWords() {
   if (storedDate === today && storedWord1 && storedWord2) {
 
     // Display stored words
-    document.getElementById('word1').innerHTML = storedWord1.toUpperCase();
-    document.getElementById('word2').innerHTML = storedWord2.toUpperCase();
+    document.getElementById('word1').innerHTML = `<p> ${storedWord1.toUpperCase()}</p>`;
+    document.getElementById('word2').innerHTML = `<p> ${storedWord2.toUpperCase()}</p>`;
   } else {
     let randomWord1, randomWord2;
 
@@ -36,38 +36,3 @@ async function generateWords() {
 
 // Call the function once when the page loads
 generateWords();
-
-
-
-
-
-
-
-
-
-// //Generate two words every new day
-// // Get the current date
-// var today = new Date();
-// var dd = String(today.getDate()).padStart(2, '0');
-// var mm = String(today.getMonth() + 1).padStart(2, '0');
-// var yyyy = today.getFullYear();
-// var currentDate = yyyy + '-' + mm + '-' + dd;
-
-// // Check if the words were already generated today
-// if (localStorage.getItem('wordsDate') === currentDate) {
-//   // The words were already generated today, so display them
-//   document.getElementById('word1').textContent = localStorage.getItem('word1');
-//   document.getElementById('word2').textContent = localStorage.getItem('word2');
-// } else {
-//   // Generate two new words and store them in local storage
-//   var word1 = words[Math.floor(Math.random() * words.length)];
-//   var word2 = words[Math.floor(Math.random() * words.length)];
-//   while (word2 === word1) {
-//     word2 = words[Math.floor(Math.random() * words.length)];
-//   }
-//   document.getElementById('word1').textContent = word1;
-//   document.getElementById('word2').textContent = word2;
-//   localStorage.setItem('word1', word1);
-//   localStorage.setItem('word2', word2);
-//   localStorage.setItem('wordsDate', currentDate);
-// }
